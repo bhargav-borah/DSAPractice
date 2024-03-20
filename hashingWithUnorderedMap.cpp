c1fe67c1ccaf591af28d6697f576ba61ddc7ea9b
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-03-20 14:11:09 +0530

    Add files via upload

---
+++ hashingWithUnorderedMap.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "bhargav";
+    
+    unordered_map<char, int> mpp;
+    for (int i = 0; i < s.size(); i++) {
+        mpp[s[i]]++;
+    }
+    
+    char q;
+    cin >> q;
+    
+    cout << q << " appears " << mpp[q] << " times." << endl;
+    
+    // The key-value pairs are stored in an unordered manner.
+    for (auto it: mpp) {
+        cout << it.first << " -> " << it.second << endl;
+    }
+}
