4ba411cd56ae7f786e33925a8584297893ed4674
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-03-21 14:08:35 +0530

    Add files via upload

---
+++ LC1752CheckIfArraysIsSortedAndRotated.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) count++;
+        }
+        if (nums[0] < nums[n - 1]) count++;
+
+        return count <= 1;
+    }
+};
