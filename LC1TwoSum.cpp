76a48a2da38e0cf910c948c4ac8d15fda713b0ef
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-03-24 15:17:55 +0530

    Add files via upload

---
+++ LC1TwoSum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        sort(nums.begin(), nums.end());
+
+        while (left <= right) {
+            if (nums)
+        }
+    }
+};
