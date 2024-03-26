f3792570163bcf6dec159f2114aeee259f3647d6
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-03-27 00:24:51 +0530

    Add files via upload

--- LC121BestTimeToBuyAndSellStocks.cpp
+++ LC121BestTimeToBuyAndSellStocks.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int mini = prices[0];
-        int maxProfit = 0;
+        int profit = 0;
 
         for (int i = 1; i < prices.size(); i++) {
-            maxProfit = max(maxProfit, prices[i] - mini);
+            profit = max(profit, prices[i] - mini);
             mini = min(mini, prices[i]);
         }
 
-        return maxProfit;
+        return profit;
     }
 };
