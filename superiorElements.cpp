7683fa7deb0b1d0ca79c72698e7fdfe261bf72c0
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-03-28 16:30:54 +0530

    Add files via upload

---
+++ superiorElements.cpp
@@ -0,0 +1,33 @@
+vector<int> superiorElements(vector<int>&a) {
+    int n = a.size();
+
+    vector<int> res;
+    int maxi = a[n - 1];
+    res.push_back(maxi);
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (a[i] > maxi) {
+            maxi = a[i];
+            res.push_back(maxi);
+        }
+    }
+
+    return res;
+}
+
+// Brute force solution
+vector<int> superiorElements(vector<int>&a) {
+    int n = a.size();
+    vector<int> res;
+    res.push_back(a[n - 1]);
+
+    for (int i = n - 2; i >= 0; i--) {
+        bool isSuperior = true;
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] <= a[j]) isSuperior = false;
+        }
+        if (isSuperior) res.push_back(a[i]);
+    }
+
+    return res;
+}
