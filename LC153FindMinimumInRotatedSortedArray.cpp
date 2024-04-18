0c7eb398a26b73ee48b274b54fcd270231f95c22
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-04-18 14:06:54 +0530

    Add files via upload

---
+++ LC153FindMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,19 @@
+int findMin(vector<int>& nums) {
+        int low = 0, high = nums.size();
+        int mini = INT_MAX;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[low] <= nums[mid]) { // when left half of search space is sorted
+                mini = min(mini, nums[low]);
+                low = mid + 1;
+            }   
+            else {                        // when right half of search space is sorted
+                mini = min(mini, nums[mid]);
+                high = mid - 1;
+            }
+        }
+
+        return mini;
+    }
