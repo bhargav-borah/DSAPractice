95fb1aae067fcd1a33318b2e41b1ad9cb8f70ef2
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-05-17 20:29:34 +0530

    Add files via upload

--- LC206ReverseLinkedList.cpp
+++ LC206ReverseLinkedList.cpp
@@ -17,7 +17,22 @@ public:
         return head;
     }
 };
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
 
+        ListNode* newHead = reverseList(head->next);
+
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+
+        return newHead;
+    }
+};
 // class Solution {
 // public:
 //     ListNode* reverseList(ListNode* head) {
