a4e7dc7e01253987b0bf0c1a87349e096cd5a8ac
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-05-26 12:38:18 +0530

    Add files via upload

--- LC232ImplementQueueUsingStacks.cpp
+++ LC232ImplementQueueUsingStacks.cpp
@@ -1,38 +1,86 @@
 class MyQueue {
 public:
-    stack<int> s1, s2;
+    stack<int> input, output;
 
     MyQueue() {
         
     }
     
     void push(int x) {
-
-        while (!s1.empty()) {
-            s2.push(s1.top());
-            s1.pop();
-        }
-
-        s1.push(x);
-
-        while (!s2.empty()) {
-            s1.push(s2.top());
-            s2.pop();
-        }
+        input.push(x);
     }
     
     int pop() {
-        int n = s1.top();
-        s1.pop();
+        if (!output.empty()) {
+            int n = output.top();
+            output.pop();
 
-        return n;
+            return n;
+        }
+
+        else {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+            int n = output.top();
+            output.pop();
+
+            return n;
+        }
     }
     
     int peek() {
-        return s1.top();
+        if (!output.empty()) return output.top();
+        else {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+            return output.top();
+        }
     }
     
     bool empty() {
-        return s1.empty();
+        return input.empty() && output.empty();
     }
 };
+
+// class MyQueue {
+// public:
+//     stack<int> s1, s2;
+
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int x) {
+
+//         while (!s1.empty()) {
+//             s2.push(s1.top());
+//             s1.pop();
+//         }
+
+//         s1.push(x);
+
+//         while (!s2.empty()) {
+//             s1.push(s2.top());
+//             s2.pop();
+//         }
+//     }
+    
+//     int pop() {
+//         int n = s1.top();
+//         s1.pop();
+
+//         return n;
+//     }
+    
+//     int peek() {
+//         return s1.top();
+//     }
+    
+//     bool empty() {
+//         return s1.empty();
+//     }
+// };
